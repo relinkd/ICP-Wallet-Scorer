34b913538d671ba924b0d79cc5af4c346ea463ad
Leo <leosungun@gmail.com>
2023-10-08 01:39:10 +0300

    add poap to param array

--- src/params/index.ts
+++ src/params/index.ts
@@ -1,7 +1,9 @@
 import { guild } from "./guild";
 import { degenScore } from "./degenScore";
+import { poap } from "./poap";
 
 export const params = [
     guild,
-    degenScore
+    degenScore,
+    poap,
 ]
