336f552af9135b12cccb7ba934455cdac3925565
Leo <leosungun@gmail.com>
2023-10-09 16:33:16 +0300

    ethDenver param

---
+++ src/params/ethDenver.ts
@@ -0,0 +1,40 @@
+import {
+	Opt, ic, match, float32
+} from 'azle';
+import {
+	managementCanister,
+} from 'azle/canisters/management';
+
+
+export const ethDenver = async (address: string): Promise<float32> => {
+    const response = await managementCanister
+		.http_request({
+			url: `https://api.poap.tech/actions/scan/${address}/103093`,
+			max_response_bytes: Opt.Some(4_000n),
+			method: {
+				'get': null,
+			},
+			headers: [
+                {
+                    name: 'accept',
+                    value: 'application/json',
+                },
+                {
+                    name: 'x-api-key',
+                    value: process.env.POAP_API_KEY!,
+                }
+            ],
+			body: Opt.None,
+			transform: Opt.Some({
+				function: [ic.id(), 'xkcdTransform'],
+				context: Uint8Array.from([]),
+			}),
+		})
+		.cycles(50_000_000n)
+		.call();
+
+	return match(response, {
+		Ok: (response) => 2,
+		Err: (err) => 0
+	});
+}
--- src/params/index.ts
+++ src/params/index.ts
@@ -3,11 +3,13 @@ import { degenScore } from "./degenScore";
 import { poap } from "./poap";
 import { lens } from "./lens";
 import { ethCC } from "./ethCC";
+import { ethDenver } from "./ethDenver";
 
 export const params = [
     guild,
     degenScore,
     ethCC,
+    ethDenver,
     // lens,
     // poap,
 ]
