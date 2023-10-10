f744bed40417d58333b677f33924f974962e17c1
Leontiy <leosungun@gmail.com>
2023-10-10 15:40:22 +0300

    gitcoin param fix

--- src/params/gitcoinTag.ts
+++ src/params/gitcoinTag.ts
@@ -34,12 +34,6 @@ export const gitcoinTag = async (address: string): Promise<float32> => {
 	
 	const decodedData = response.Ok?.body && JSON.parse(decodeUtf8(response.Ok?.body));
 
-	console.log(JSON.stringify(response), JSON.stringify({headers: [
-        {
-            name: 'X-API-KEY',
-            value: process.env.GITCOIN_KEY!,
-        }
-    ]}))
 
 	return match(response, {
 		Ok: (response) => decodedData?.items?.length * 0.5,
