107dc0b81bb2c72169026a615fa62279b737848e
Leo <leosungun@gmail.com>
2023-10-15 13:57:18 +0300

    xkcdRaw remove

--- src/index.ts
+++ src/index.ts
@@ -27,35 +27,6 @@ export async function xkcd(address: text): Promise<Manual<float32>> {
     ic.reply(score);
 }
 
-// TODO the replica logs give some concerning output: https://forum.dfinity.org/t/fix-me-in-http-outcalls-call-raw/19435
-$update;
-export async function xkcdRaw(): Promise<Manual<text>> {
-    const httpResult = await ic.callRaw(
-        Principal.fromText('aaaaa-aa'),
-        'http_request',
-        ic.candidEncode(`
-            (
-                record {
-                    url = "https://xkcd.com/642/info.0.json";
-                    max_response_bytes = 2_000 : nat64;
-                    method = variant { get };
-                    headers = vec {};
-                    body = null;
-                    transform = record { function = func "${ic
-                        .id()
-                        .toString()}".xkcdTransform; context = vec {} };
-                }
-            )
-        `),
-        50_000_000n
-    );
-
-    match(httpResult, {
-        Ok: (httpResponse) => ic.reply(ic.candidDecode(httpResponse)),
-        Err: (err) => ic.reply('Error'),
-    });
-}
-
 $query;
 export function xkcdTransform(args: HttpTransformArgs): HttpResponse {
     return {
